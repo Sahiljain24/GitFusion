2def4c7de975372a45cac86f3cacea2d7d79402e
Sahiljain24 <24sahilj@gmail.com>
2024-08-03 01:17:26 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
+import ExplorePage from "./Pages/ExplorePage";
+import LikePage from "./Pages/LikePage";
+
+
  
  
 
@@ -5,7 +13,18 @@ function App() {
  
   return (
      <div className='text-red-600 bg-red-950'>
-       hello world 
+       hello
+       <Sidebar/>
+         <div>
+           <Routes>
+             <Route path="/" element={<HomePage/>} />
+             <Route path="/SignUp" element={<SignUpPage/>} />
+             <Route path="/Login" element={<LoginPage/>} />
+             <Route path="/Explore" element={<ExplorePage/>} />
+             <Route path="/likes" element={<LikePage/>} />
+           </Routes>
+         </div>
+        
      </div>
          
   )

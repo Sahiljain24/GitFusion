bb6b00f87f1e0429dc7e81825c9865a61cf7955d
Sahiljain24 <24sahilj@gmail.com>
2024-08-02 20:51:31 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ 
+ 
 
 function App() {
  
   return (
-     <div>
-      <H1>Hello world</H1>
+     <div className='text-red-600 bg-red-950'>
+       hello world 
      </div>
          
   )

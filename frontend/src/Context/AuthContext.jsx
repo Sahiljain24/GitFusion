1f4b932530af1c0c3aac2711e673754dcec3efe5
Sahiljain24 <24sahilj@gmail.com>
2024-08-07 22:24:37 +0530

    update

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,37 +1,37 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {toast} from "react-hot-toast"
+import { toast } from "react-hot-toast";
 
 export const AuthContext = createContext();
-export const useAuthContext=()=>{
-    return useContext(AuthContext);
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
 }
 
-const AuthContextProvider =({props})=>{
-       
-    const [authUser,setAuthUser] =useState(nul);
-    const [loading,setLoading] =useState(true);
-     
-    useEffect( ()=>{
-        const checkUserLoggedIn=async()=>{
-        setLoading(true)
-        try{
-            const res= await fetch("/api/auth/check",{credentials:"include"});
-            const data = res.json();
-            setAuthUser(data.user);
-        }
-        catch(err){
-            toast.error(err);
-           }
-           finally{
-            setLoading(false);
-           }
-        }
-        checkUserLoggedIn();
-    },[]);
+const AuthContextProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <AuthContext.Provider value={{setAuthUser,authUser,loading}}>
-            {props}
-        </AuthContext.Provider>
-    )
+  useEffect(() => {
+    const checkUserLoggedIn = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/auth/check", { credentials: "include" });
+        const data = await res.json();
+        setAuthUser(data.user);
+      } catch (err) {
+        toast.error(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    checkUserLoggedIn();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ setAuthUser, authUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
+
+export default AuthContextProvider;
